38769483f2caa89cd80a794a4de32080d910ad7c
Sukhendu Ghosh <sgeightyone@gmail.com>
2023-07-02 13:28:08 +0530

    Remove dist and node_modules directories from repository

--- dist/middlewares/logger.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context, Next } from 'koa';
-declare const logger: (ctx: Context, next: Next) => Promise<void>;
-export default logger;
--- dist/src/controllers/authController.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context } from 'koa';
-export declare const login: (ctx: Context) => Promise<void>;
-export declare const logout: (ctx: Context) => Promise<void>;
